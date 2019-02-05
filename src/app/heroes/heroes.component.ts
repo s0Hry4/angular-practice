aa99ed0e0bdc5855de6d410c90260d126869b19f
s_sonoda <>
2019-02-05 11:26:46 +0900

    first commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero'; // hero
+import { HEROES } from '../mock-heroes'; // DBから取得した体のデータ
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+
+export class HeroesComponent implements OnInit {
+
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  constructor(private heroService: HeroService) { }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
+
+
+/*
+Copyright 2017-2018 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,23 @@
+
+
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'ワンパンマン' },
+  { id: 2, name: '' },
+  { id: 3, name: 'ボンバーストーム' },
+  { id: 4, name: 'セリヌンティウス' },
+  { id: 5, name: 'マグネーター' },
+  { id: 6, name: 'ラビットマン' },
+  { id: 7, name: 'ダイナマン' },
+  { id: 8, name: 'ドクターIQ' },
+  { id: 9, name: 'マグママン' },
+  { id: 10, name: 'トルナンドマン' }
+];
+
+
+/*
+Copyright 2017-2018 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
